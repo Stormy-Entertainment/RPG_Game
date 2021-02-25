b89a1f38321e19f52b2a3b27826f450496183c40
Ken Soun <66926061+nouSneK@users.noreply.github.com>
2021-02-25 11:20:13 +1100

    Added User Interface (Health Bar)

---
+++ Assets/Scripts/PlayerController_Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Script : MonoBehaviour
+{
+    public float Health = 100f;
+
+    // player movement code and damage detector goes
+    // here so that you can control the player health
+
+}
