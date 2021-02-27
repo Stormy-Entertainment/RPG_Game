25ad11414243afa52fccaadc245e7ccbbb4a677e
Elejarken16 <72119579+Elejarken16@users.noreply.github.com>
2021-02-27 20:16:33 +1100

    flamethrower stage haha

---
+++ Assets/Projectile/MoveProjectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectile : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speed != 0)
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("No Speed");
+        }
+    }
+
+    private void OnCollisionEnter(Collision co)
+    {
+        speed = 0;
+
+        Destroy(gameObject);
+    }
+}
