50b3506213ca41a249ac792bb7c515cf48fa8fc2
keith1755 <62937410+keith1755@users.noreply.github.com>
2021-03-04 11:06:25 +1100

    AI enemy
    Wandering
    Chasing player
    Attacking (just method and show message now, require atk script)
    #Showing sight and attack range
    
    Not yet complete, still fixing the "lookAt" problem

---
+++ Assets/Level prototype/EnemyAI.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Defence point
+    public Transform defPoint;
+
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Hero").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Defence();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+
+    }
+
+
+    private void Defence()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, defPoint.position, 0.1f);
+
+        //transform.LookAt(defPoint);
+    }
+
+
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+
+        if (!alreadyAttacked)
+        {
+            print("hit");
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+
+    }
+}
