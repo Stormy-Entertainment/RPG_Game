a003ab7564f51b715001158c06aecfde3e0b1301
Josh Philipson <1018643@student.sae.edu.au>
2021-03-11 10:33:37 +1100

    readded quickoutline

---
+++ Assets/Scripts/QuickOutline/ObjectOutline.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOutline : MonoBehaviour
+{
+
+    public Outline myScript;
+
+    void Start()
+    {
+        myScript = GetComponent<Outline>();
+        myScript.enabled = false;
+
+    }
+
+    public void OnMouseOver()
+    {
+        myScript.enabled = true;
+    }
+    public void OnMouseExit()
+    {
+        myScript.enabled = false;
+    }
+
+
+}
