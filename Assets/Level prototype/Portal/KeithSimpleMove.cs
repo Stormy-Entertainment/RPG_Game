6f33f0cc0c5b2b99efad4b63c387010ad6ee2fe2
keith1755 <62937410+keith1755@users.noreply.github.com>
2021-03-16 00:20:56 +1100

    portal improve
    +particle effect
    +sound
    +message

---
+++ Assets/Level prototype/Portal/KeithSimpleMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeithSimpleMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed;
+    public float rotateSpeed;
+    public bool isJumping;
+
+    public float gravity;
+    public float jump;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    //float turnSmoothVelocity;
+    Vector3 velocity;
+
+    void Update()
+    {
+        velocity.y += -gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        //A,D rotate
+        float rotation = Input.GetAxisRaw("Horizontal") * rotateSpeed;
+
+        //W,S move forward and backward
+        float moveFB = Input.GetAxisRaw("Vertical") * speed;
+
+        moveFB *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, moveFB);
+        transform.Rotate(0, rotation, 0);
+
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -0.1f;
+        }
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        print("jump");
+        velocity.y += Mathf.Sqrt(jump * -2f * -gravity);
+        isJumping = true;
+
+    }
+
+}
---
+++ Assets/Level prototype/Portal/Portal2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal2 : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+    public GameObject info;
+    public GameObject pEffect;
+    public AudioSource audioSource;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("stay");
+            info.SetActive(true);
+            info.transform.LookAt(Camera.main.transform);
+            pEffect.SetActive(true);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                thePlayer.transform.position = teleportTarget.transform.position;
+                audioSource.Play();
+                print("go");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            print("stay");
+            info.SetActive(false);
+            pEffect.SetActive(false);
+            info.transform.LookAt(Camera.main.transform);
+        }
+
+    }
+}
