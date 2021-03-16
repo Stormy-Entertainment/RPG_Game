a32d85dc7d3eeba34b2fc2cf9d065939bf3c6532
Amit Thapa <gameonamit@gmail.com>
2021-03-16 14:25:49 +1100

    Problem Fixed
    Problem Fixed

---
+++ Assets/Scripts/Systems/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	public static Inventory instance;
+
+	public int money;
+	// Item gets added/removed.
+	public delegate void OnItemChanged();
+	public OnItemChanged onItemChangedCallback;
+
+	public int space = 20;  // Amount of slots in the inventory
+
+	// List of items in inventory
+	public List<Item> items = new List<Item>();
+
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			return;
+		}
+		instance = this;
+	}
+
+	// Add a new item
+	public bool Add(Item item)
+	{
+		if (!item.isDefaultItem)
+		{
+			// Check if out of space
+			if (items.Count >= space)
+			{
+				Debug.Log("Not enough room.");
+				return false;
+			}
+
+			items.Add(item);
+
+			if (onItemChangedCallback != null)
+				onItemChangedCallback.Invoke();
+		}
+		return true;
+	}
+
+	// Remove an item
+	public void Remove(Item item)
+	{
+		items.Remove(item);
+
+		if (onItemChangedCallback != null)
+			onItemChangedCallback.Invoke();
+	}
+}
