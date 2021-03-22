33a805653d2b7f180cc75f3f29f6e1ab795f1cda
Josh Philipson <1018643@student.sae.edu.au>
2021-03-22 15:03:55 +1100

    Cutscene

---
+++ Assets/Cutscene Assets/SceneSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSequence : MonoBehaviour
+{
+    public GameObject Cam1;
+    public GameObject playerCam;
+
+    void Start()
+    {
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence()
+    {
+        yield return new WaitForSeconds(6);
+        Cam1.SetActive(false);
+        playerCam.SetActive(true);
+    }
+
+
+
+}
