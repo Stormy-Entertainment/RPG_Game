fa7a16090f8198d6146a062f7203067ae5b489f3
Amit Thapa <gameonamit@gmail.com>
2021-04-11 13:09:06 +1000

    new

--- Assets/Scripts/Level Scripts/CoinDrop.cs
+++ Assets/Scripts/Level Scripts/CoinDrop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CoinDrop : MonoBehaviour
 {
+    
     private Transform Target;
     public float Speed = 1.5f;
     public int CoinsPerItem = 100;
@@ -11,48 +12,24 @@ public class CoinDrop : MonoBehaviour
     Vector3 _velocity = Vector3.zero;
     bool _isFollowing = false;
 
-    public float playerFollowRadius = 2f;
-    public LayerMask playerLayerMask;
-    private RaycastHit hit;
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        if (_isFollowing && Target != null)
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref _velocity, Time.deltaTime * Speed);
-            //transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * Speed);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        CheckPlayerCollision();
+        FollowingPlayer();
     }
 
     private void FollowingPlayer()
     {
-        if (!_isFollowing)
-        {
-            Target = GameObject.FindWithTag("Player").GetComponentInChildren<PlayerTarget>().transform;
-            _isFollowing = true;
-        }
+        Target = GameObject.FindWithTag("Player").GetComponentInChildren<PlayerTarget>().transform;
+        _isFollowing = true;
     }
 
-    private void CheckPlayerCollision()
+    // Update is called once per frame
+    void Update()
     {
-        if (Physics.SphereCast(transform.position, playerFollowRadius, Vector3.up, out hit, 20f, playerLayerMask))
+        if (_isFollowing && Target!=null)
         {
-            //Debug.Log(hit.collider.gameObject.name);
-            //using Sphere Cast for Collison Check
-            if (hit.collider.gameObject.tag == "Player")
-            {
-                FollowingPlayer();
-            }
-        }
-        else
-        {
-            _isFollowing = false;
+            transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref _velocity, Time.deltaTime * Speed);
+            //transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * Speed);
         }
     }
 
@@ -64,11 +41,5 @@ public class CoinDrop : MonoBehaviour
             other.GetComponentInParent<Inventory>().IncreaseCoins(CoinsPerItem);
             Destroy(this.gameObject);
         }
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, playerFollowRadius);
     }
 }
