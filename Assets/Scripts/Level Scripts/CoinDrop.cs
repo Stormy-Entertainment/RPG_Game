75c6d7c1aa89b3352d9918512931e9f5b83f9345
Amit Thapa <gameonamit@gmail.com>
2021-04-11 13:10:16 +1000

    Revert "new"
    This reverts commit fa7a16090f8198d6146a062f7203067ae5b489f3.

--- Assets/Scripts/Level Scripts/CoinDrop.cs
+++ Assets/Scripts/Level Scripts/CoinDrop.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CoinDrop : MonoBehaviour
 {
-    
     private Transform Target;
     public float Speed = 1.5f;
     public int CoinsPerItem = 100;
@@ -12,24 +11,48 @@ public class CoinDrop : MonoBehaviour
     Vector3 _velocity = Vector3.zero;
     bool _isFollowing = false;
 
-    private void Start()
-    {
-        FollowingPlayer();
-    }
-
-    private void FollowingPlayer()
-    {
-        Target = GameObject.FindWithTag("Player").GetComponentInChildren<PlayerTarget>().transform;
-        _isFollowing = true;
-    }
+    public float playerFollowRadius = 2f;
+    public LayerMask playerLayerMask;
+    private RaycastHit hit;
 
     // Update is called once per frame
     void Update()
     {
-        if (_isFollowing && Target!=null)
+        if (_isFollowing && Target != null)
         {
             transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref _velocity, Time.deltaTime * Speed);
             //transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * Speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckPlayerCollision();
+    }
+
+    private void FollowingPlayer()
+    {
+        if (!_isFollowing)
+        {
+            Target = GameObject.FindWithTag("Player").GetComponentInChildren<PlayerTarget>().transform;
+            _isFollowing = true;
+        }
+    }
+
+    private void CheckPlayerCollision()
+    {
+        if (Physics.SphereCast(transform.position, playerFollowRadius, Vector3.up, out hit, 20f, playerLayerMask))
+        {
+            //Debug.Log(hit.collider.gameObject.name);
+            //using Sphere Cast for Collison Check
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                FollowingPlayer();
+            }
+        }
+        else
+        {
+            _isFollowing = false;
         }
     }
 
@@ -41,5 +64,11 @@ public class CoinDrop : MonoBehaviour
             other.GetComponentInParent<Inventory>().IncreaseCoins(CoinsPerItem);
             Destroy(this.gameObject);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, playerFollowRadius);
     }
 }
