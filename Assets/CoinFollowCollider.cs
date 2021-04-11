72db74a6e113ad66ddfad52beb34ffa90c10dc24
Amit Thapa <gameonamit@gmail.com>
2021-04-11 17:48:29 +1000

    Coin Pickup Working
    Got coin system to finally work after switching from spherecast to using sphere colliders instead.

---
+++ Assets/CoinFollowCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinFollowCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponentInParent<CoinDrop>().FollowPlayer();
+        }
+    }
+}
