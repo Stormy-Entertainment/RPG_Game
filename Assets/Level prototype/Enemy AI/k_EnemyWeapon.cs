1dc3c37913b8aa75eeabbbfb1bc696af81dfb889
keith1755 <62937410+keith1755@users.noreply.github.com>
2021-04-15 10:46:38 +1000

    add the missing files

---
+++ Assets/Level prototype/Enemy AI/k_EnemyWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class k_EnemyWeapon : MonoBehaviour
+{
+    //Attach this script to the weapon, this object collider will active when the attack animation played 
+    public float attackDamage;
+    private GameObject player;
+    public AudioSource audio;
+
+    void OnTriggerEnter(Collider other)
+    {
+        player = GameObject.Find("Player");
+
+        if (other.gameObject.tag == "Player")
+        {
+            audio = GetComponent<AudioSource>();
+            GetComponent<AudioSource>().Play();
+
+            player.GetComponent<k_hpCon>().DecreaseHealth(attackDamage);
+            Debug.Log("Hit");
+        }
+    }
+}
