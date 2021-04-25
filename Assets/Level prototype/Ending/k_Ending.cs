29dfe550346b7bd1ce0c3fd7f6adebd47f706ae8
keith1755 <62937410+keith1755@users.noreply.github.com>
2021-04-25 23:37:04 +1000

    Ending Scene (test)

---
+++ Assets/Level prototype/Ending/k_Ending.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class k_Ending : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject sound1;
+    public GameObject victory;
+
+    public AudioClip victorySound;
+
+    public TextMeshProUGUI textDisplay;
+    public string sentence;
+    private int index;
+    public float typingSpeed;
+    public GameObject nextButton;
+
+    public AudioSource audio;
+    public int sceneNumber;
+
+    public void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        
+    }
+
+    public void Bomb()
+    {
+        Debug.Log("B");
+        explosion.SetActive(true);
+    }
+
+    public void BossDie()
+    {
+        sound1.SetActive(true);
+    }
+
+    public void WinText()
+    {
+        victory.SetActive(true);
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentence.ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+            nextButton.SetActive(true);
+        }
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
