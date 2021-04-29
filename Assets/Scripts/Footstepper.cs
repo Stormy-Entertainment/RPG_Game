8054e0182671c9ada8c3f18d2fff0f149eaf4337
xPhilo1 <1018643@student.sae.edu.au>
2021-04-29 10:23:52 +1000

    Footstepper setup

--- Assets/Scripts/Footstepper.cs
+++ Assets/Scripts/Footstepper.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Footstepper : MonoBehaviour
 {
+    public LayerMask Ground;
 
-    public float Length;
     private AudioSource m_AudioSource;
 
     public AudioClip[] m_stoneFootSounds;
-    public AudioClip[] m_WoodFootSounds;
+    
 
 
 
     private void Start(){
         m_AudioSource = GetComponent<AudioSource>();
     }
-    private void OnTriggerEnter(Collider other){
-            if(other.gameObject.CompareTag("Landmark")) {
+    private void OnCollisionEnter(Collision other){
+        Debug.Log("hello 1");
+        if (other.gameObject.layer == Ground)
+        {
+            Debug.Log("hello 2");
             int n = Random.Range(1, m_stoneFootSounds.Length);
             m_AudioSource.clip = m_stoneFootSounds[n];
             m_AudioSource.PlayOneShot(m_AudioSource.clip);
@@ -25,15 +28,7 @@ public class Footstepper : MonoBehaviour
             m_stoneFootSounds[n] = m_stoneFootSounds[0];
             m_stoneFootSounds[0] = m_AudioSource.clip;
         }
-        if (other.gameObject.CompareTag("Wood"))
-        {
-            int n = Random.Range(1, m_WoodFootSounds.Length);
-            m_AudioSource.clip = m_WoodFootSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
-            // move picked sound to index 0 so it's not picked next time
-            m_WoodFootSounds[n] = m_WoodFootSounds[0];
-            m_WoodFootSounds[0] = m_AudioSource.clip;
-        }
+        
     }
 
 }
